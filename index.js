1c3f86685d218fabe75e4770fa151ae8e0b6afc7
Ashley Davis <ashley.davis@realseriousgames.com>
2015-06-10 16:55:15 +1000

    Build file in the working directory now overrides.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 module.exports = function (config) {
+
+	var buildFilePath = path.join(process.cwd(), "build.js");
+	console.log(buildFilePath);
 	
-	var result = require("./build.js")({}, {}, {});
+	var result = require(buildFilePath)({}, {}, {});
 	console.log(result.test);
 };
 
