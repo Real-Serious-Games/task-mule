0823294c65d6e34a047e5648ca4509c40555584d
Ashley Davis <ashley.davis@realseriousgames.com>
2016-01-13 14:16:19 +1000

    Refactored each task-mule command to a separate function.

--- index.js
+++ index.js
@@ -1,107 +1,110 @@
 'use strict';
 
-module.exports = function (config) {
+var argv = require('yargs').argv;
+var conf = require('confucious');
+var path = require('path');
+var fs = require('fs-extra');
+var globby = require('globby');
+var chalk = require('chalk');
+var validate = require('./validate');
+var S = require('string');
+var AsciiTable = require('ascii-table');
 
-	var argv = require('yargs').argv;
-	var conf = require('confucious');
-	var path = require('path');
-	var fs = require('fs-extra');
-	var globby = require('globby');
-	var chalk = require('chalk');
-	var validate = require('./validate');
-	var S = require('string');
-	var AsciiTable = require('ascii-table');
+var workingDirectory = process.cwd();
+var buildFilePath = path.join(workingDirectory, "build.js");
+var tasksDirectory = path.join(workingDirectory, 'tasks');
 
-	config = config || {};
+//
+// task-mule init
+//
+var commandInit = function (log) {
 
-	var log = config.log || require('./log')(argv.verbose, argv.nocolors);
+	if (fs.existsSync(buildFilePath)) {
+		log.error("Can't overwrite existing 'build.js'.");
+		process.exit(1);
+	}
 
-	global.runCmd = require('./run-cmd')(log);
-	global.path = require('path');
-	global.fs = require('fs-extra');
-	global.quote = require('quote');
+	// Auto create build.js.
+	var defaultBuildJs = path.join(__dirname, 'build.js');
+	fs.copySync(defaultBuildJs, buildFilePath);
+	log.info("Created new 'build.js' at " + buildFilePath);
+	process.exit(0);
+};
 
-	var workingDirectory = process.cwd();
-	var buildFilePath = path.join(workingDirectory, "build.js");
-	var tasksDirectory = path.join(workingDirectory, 'tasks');
+//
+// task-mule create-task <task-name>
+//
+var commandCreateTask = function (log) {
+
+	var newTaskName = argv._[1];
+	if (!newTaskName) {
+		log.error("Task name not specified.");
+		process.exit(1);
+	}
+
+	if (!S(newTaskName.toLowerCase()).endsWith(".js")) {
+		if (newTaskName[newTaskName.length-1] === '.') {
+			// Trim final period.
+			newTaskName = newTaskName.substring(0, newTaskName.length-1);
+		}
+		
+		// Auto add extension.
+		newTaskName += ".js";
+	}
+
+	var newTaskFilePath = path.join(tasksDirectory, newTaskName);
+	if (fs.existsSync(newTaskFilePath)) {
+		log.error("Can't create task, file already exists: " + newTaskFilePath);
+		process.exit(1);
+	}
+
+	var defaultTaskFile = path.join(__dirname, 'default-task.js');
+	fs.copySync(defaultTaskFile, newTaskFilePath);
+	log.info("Created new task file at " + newTaskFilePath);
+};
+
+//
+// task-mule schedule
+//
+var commandSchedule = function (log) {
+
+	if (!fs.existsSync('schedule.json')) {
+		log.error('Expected scehdule.json to specify the schedule of tasks.');
+		process.exit(1);
+	}
+
+	var buildConfig = require(buildFilePath)(conf, log, validate);
+
+	var defaultConfigFilePath = path.join(workingDirectory, 'config.json');
+	if (fs.existsSync(defaultConfigFilePath)) {
+
+		log.verbose("Loading config from file: " + defaultConfigFilePath);
+
+		conf.pushJsonFile(defaultConfigFilePath);
+	}
+
+	conf.pushEnv();
+	conf.pushArgv();
+
+	if (config.defaultConfig) {
+		conf.push(config.defaultConfig)
+	}
+
+	buildConfig.init();
+
+	var taskRunner = require('./task-loader.js')({}, log, validate, conf);
+
+	var schedule = JSON.parse(fs.readFileSync('schedule.json', 'utf8'));
+
+	var taskScheduler = require('task-scheduler');
+	taskScheduler.start(schedule, config.schedulerCallbacks);
+};
+
+//
+// task-mule <task-name>
+//
+var commandRunTask = function (log, requestedTaskName) {
 	
-	var requestedTaskName = config.requestedTaskName || argv._[0];
-	if (requestedTaskName === 'init') {
-		if (fs.existsSync(buildFilePath)) {
-			log.error("Can't overwrite existing 'build.js'.");
-			process.exit(1);
-		}
-
-		// Auto create build.js.
-		var defaultBuildJs = path.join(__dirname, 'build.js');
-		fs.copySync(defaultBuildJs, buildFilePath);
-		log.info("Created new 'build.js' at " + buildFilePath);
-		process.exit(0);
-	}
-	else if (requestedTaskName === 'create-task') {
-		var newTaskName = argv._[1];
-		if (!newTaskName) {
-			log.error("Task name not specified.");
-			process.exit(1);
-		}
-
-		if (!S(newTaskName.toLowerCase()).endsWith(".js")) {
-			if (newTaskName[newTaskName.length-1] === '.') {
-				// Trim final period.
-				newTaskName = newTaskName.substring(0, newTaskName.length-1);
-			}
-			
-			// Auto add extension.
-			newTaskName += ".js";
-		}
-
-		var newTaskFilePath = path.join(tasksDirectory, newTaskName);
-		if (fs.existsSync(newTaskFilePath)) {
-			log.error("Can't create task, file already exists: " + newTaskFilePath);
-			process.exit(1);
-		}
-
-		var defaultTaskFile = path.join(__dirname, 'default-task.js');
-		fs.copySync(defaultTaskFile, newTaskFilePath);
-		log.info("Created new task file at " + newTaskFilePath);
-
-		process.exit(0);
-	}
-	else if (requestedTaskName === 'schedule') {
-
-		if (!fs.existsSync('schedule.json')) {
-			log.error('Expected scehdule.json to specify the schedule of tasks.');
-			process.exit(1);
-		}
-
-		var buildConfig = require(buildFilePath)(conf, log, validate);
-
-		var defaultConfigFilePath = path.join(workingDirectory, 'config.json');
-		if (fs.existsSync(defaultConfigFilePath)) {
-
-			log.verbose("Loading config from file: " + defaultConfigFilePath);
-
-			conf.pushJsonFile(defaultConfigFilePath);
-		}
-
-		conf.pushEnv();
-		conf.pushArgv();
-
-		if (config.defaultConfig) {
-			conf.push(config.defaultConfig)
-		}
-
-		buildConfig.init();
-
-		var taskRunner = require('./task-loader.js')({}, log, validate, conf);
-
-		var schedule = JSON.parse(fs.readFileSync('schedule.json', 'utf8'));
-
-		var taskScheduler = require('task-scheduler');
-		taskScheduler.start(schedule, config.schedulerCallbacks);
-		return;
-	}
-
 	if (!fs.existsSync(buildFilePath)) {
 		log.error("'build.js' not found, please run task-mule in a directory that has this file.");
 		log.info("Run 'task-mule init' to create a default 'build.js'.")
@@ -194,5 +197,33 @@ module.exports = function (config) {
 
 	    process.exit(1);
 	}
+};
+
+module.exports = function (config) {
+
+	config = config || {};
+
+	var log = config.log || require('./log')(argv.verbose, argv.nocolors);
+
+	global.runCmd = require('./run-cmd')(log);
+	global.path = require('path');
+	global.fs = require('fs-extra');
+	global.quote = require('quote');
+
+	var requestedTaskName = config.requestedTaskName || argv._[0];
+	if (requestedTaskName === 'init') {
+		commandInit(log);
+		process.exit(0);
+	}
+	else if (requestedTaskName === 'create-task') {
+		commandCreateTask(log);
+		process.exit(0);
+	}
+	else if (requestedTaskName === 'schedule') {
+		commandSchedule(log);
+		return;
+	}
+
+	commandRunTask(log, requestedTaskName);
 };
 
