b11eaa3305b5ed094d6a5ed088cc3849fb006691
Ashley Davis <ashley@codecapers.com.au>
2016-04-12 14:09:58 +1000

    Added a stub test.

---
+++ test/task-mule.tests.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('task-mule', function () {
+
+	var taskMule = require('../index.js');
+
+	it('task-mule can handle an unhandled exception and fail the task', function () {
+
+		taskMule({
+
+			});
+
+
+	});
+
+});
